19ead13defcb34a5365a23960ea28c23e2ff958d
Krait <krait@dqteam.org>
2017-01-26 00:14:54 +0500

    Added a stub for stat server project.

---
+++ Kontur.GameStats.Server/EntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Fclp;
+
+namespace Kontur.GameStats.Server
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            var commandLineParser = new FluentCommandLineParser<Options>();
+
+            commandLineParser
+                .Setup(options => options.Prefix)
+                .As("prefix")
+                .SetDefault("http://+:8080/")
+                .WithDescription("HTTP prefix to listen on");
+
+            commandLineParser
+                .SetupHelp("h", "help")
+                .WithHeader($"{AppDomain.CurrentDomain.FriendlyName} [--prefix <prefix>]")
+                .Callback(text => Console.WriteLine(text));
+
+            if (commandLineParser.Parse(args).HelpCalled)
+                return;
+
+            RunServer(commandLineParser.Object);
+        }
+
+        private static void RunServer(Options options)
+        {
+            using (var server = new StatServer())
+            {
+                server.Start(options.Prefix);
+
+                Console.ReadKey(true);
+            }
+        }
+
+        private class Options
+        {
+            public string Prefix { get; set; }
+        }
+    }
+}
